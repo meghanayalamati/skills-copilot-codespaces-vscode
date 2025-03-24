8a97161397c4a9f4d2e96fc9f795ab7a747f2347
meghanayalamati <v-nyalamati@microsoft.com>
2025-03-24 04:27:54 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,19 @@
+// Create web server
+// Start the server
+// Create a route for comments
+// Create a route for comments/:id
+// Create a route for comments/:id/upvote
+// Create a route for comments/:id/downvote
+// Create a route for comments/:id/delete
+// Create a route for comments/:id/edit
+// Create a route for comments/:id/update
+// Create a route for comments/:id/replies
+// Create a route for comments/:id/replies/new
+// Create a route for comments/:id/replies/:replyId
+// Create a route for comments/:id/replies/:replyId/upvote
+// Create a route for comments/:id/replies/:replyId/downvote
+// Create a route for comments/:id/replies/:replyId/delete
+// Create a route for comments/:id/replies/:replyId/edit
+// Create a route for comments/:id/replies/:replyId/update
+// Create a route for comments/:id/replies/:replyId/replies
+// Create a route for comments/:id/replies/:replyId/replies/new
